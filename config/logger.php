6b67c3e86fac84b8b9c706f8c5e278a04c46881d
Mougrim <rinat@mougrim.ru>
2018-06-21 09:52:26 +0300

    Check that "amphp/log" is installed in default logger config.

--- config/logger.php
+++ config/logger.php
@@ -9,6 +9,11 @@ use Amp\ByteStream\ResourceOutputStream;
 use Amp\Log\StreamHandler;
 use Monolog\Logger;
 use const STDOUT;
+use function class_exists;
+
+if (!class_exists(StreamHandler::class)) {
+    throw new RunError('You should install "amphp/log" by default or provide your custom config/logger.php via config for use php-xdebug-proxy');
+}
 
 return (new Logger('xdebug-proxy'))
     ->pushHandler(
